9cfeaa72bc3903cc59d3e34a1407775c80affe10
huohuade <muha.ragy@gmail.com>
2023-10-09 12:34:03 +0300

    gg

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'all good -- database is up';
+    return 'الدوكر علي بتاعي';
     return view('welcome');
 });
