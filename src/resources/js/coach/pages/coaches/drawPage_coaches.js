6675173794e11f55879c6a5f67b5901bf443ce05
huohuade <muha.ragy@gmail.com>
2023-10-30 13:05:32 +0200

    gg

--- src/resources/js/coach/pages/coaches/drawPage_coaches.js
+++ src/resources/js/coach/pages/coaches/drawPage_coaches.js
@@ -27,7 +27,7 @@ drawPage_coaches = function(){
                 ),
                 $('<td/>',{class:!window.accessibility.coaches_manage ? 'none' : ''}).append(
                     $('<div/>',{class:'row alnC jstfyS'}).append(
-                        $('<a/>',{class:'ico-wechat mX10 w25 h25 pointer',tooltip:text.main.call,href:`weixin://dl/chat?contact=e142890bus`}),
+                        $('<a/>',{class:'ico-wechat mX10 w25 h25 pointer',tooltip:text.main.call,href:`weixin://dl/chat?wxid_0ltqxz3fksd122`}),
                         $('<a/>',{class:'ico-phone mX10 w25 h25 pointer',tooltip:text.main.call,}),
                         $('<div/>',{class:'ico-activity_logs mX10 w25 h25 pointer',tooltip:text.main.activities}),
                         $('<div/>',{coach:coach.id,class:'ico-settings mX10 w25 h25 pointer showPage',page:'manage_coach',tooltip:text.main.manage}),
@@ -40,5 +40,5 @@ drawPage_coaches = function(){
 
 }
 // e142890bus
-//
+// u.wechat.com/
 //
